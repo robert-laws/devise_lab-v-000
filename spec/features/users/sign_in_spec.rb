dd12390a1ec1d43be9e338751f3ed2f9fa5709eb
Robert Laws <roblaws@me.com>
2018-02-01 10:37:54 +0000

    automatically backed up by learn

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -5,17 +5,17 @@
 #   So I can visit protected areas of the site
 
 feature 'Sign in', :devise do
-  
+
   # Scenario: User cannot sign in if not registered
   #   Given I do not exist as a user
   #   When I sign in with valid credentials
   #   Then I see an invalid credentials message
   # scenario 'user cannot sign in if not registered' do
-  # 
+  #
   #   signin('test@example.com', 'please123')
   #   expect(page).to have_content(/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   # end
-  # 
+  #
   # # Scenario: User can sign in with valid credentials
   # #   Given I exist as a user
   # #   And I am not signed in
@@ -26,7 +26,7 @@ feature 'Sign in', :devise do
   #   signin(user.email, user.password)
   #   expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   # end
-  # 
+  #
   # # Scenario: User cannot sign in with wrong email
   # #   Given I exist as a user
   # #   And I am not signed in
@@ -37,7 +37,7 @@ feature 'Sign in', :devise do
   #   signin('invalid@email.com', user.password)
   #   expect(page).to have_content(/#{I18n.t('devise.failure.not_found_in_database', authentication_keys: 'email')}/i)
   # end
-  # 
+  #
   # # Scenario: User cannot sign in with wrong password
   # #   Given I exist as a user
   # #   And I am not signed in
