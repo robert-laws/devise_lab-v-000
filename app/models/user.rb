3edb214dba5e798727aaa3ae5576c5578b587d09
Robert Laws <roblaws@me.com>
2018-02-01 10:27:48 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :omniauthable, :omniauth_providers => [:facebook]
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
